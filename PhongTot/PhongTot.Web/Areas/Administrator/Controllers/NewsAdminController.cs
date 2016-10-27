ac8d6d82995c62fda56701014fa943f3fb6b5573
LeChiLuc <leluc276@gmail.com>
2016-10-27 19:53:35 +0700

    add AdminPost

---
+++ PhongTot/PhongTot.Web/Areas/Administrator/Controllers/NewsAdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PhongTot.Web.Areas.Administrator.Controllers
+{
+    public class NewsAdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
