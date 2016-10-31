012fbe92da3dc2dccd05ad71136789224223564c
LeChiLuc <leluc276@gmail.com>
2016-10-31 21:33:34 +0700

    binding list post+ page

---
+++ PhongTot/PhongTot.Web/Areas/Administrator/Controllers/PostAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhongTot.Web.Areas.Administrator.Controllers
+{
+    public class PostAdminController : Controller
+    {
+        // GET: Administrator/PostAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
