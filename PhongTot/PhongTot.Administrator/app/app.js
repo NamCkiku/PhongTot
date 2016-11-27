3bc48b382c0e321b92b6c94a6028b2ee769ce97c
NamCkiku <tranhoangnam11373@gmail.com>
2016-11-27 12:19:37 +0700

    add add news

--- PhongTot/PhongTot.Administrator/app/app.js
+++ PhongTot/PhongTot.Administrator/app/app.js
@@ -1,6 +1,6 @@
 ﻿/// <reference path="E:\_Study\_SourceCode\_SourseGithub\PhongTot\PhongTot\PhongTot.Administrator\bower_components/angular/angular.js" />
 (function () {
-    angular.module('myApp', ['ui.router', 'blockUI']).config(config);
+    angular.module('myApp', ['ui.router', 'blockUI', 'ngCkeditor', 'ngTagsInput', 'frapontillo.bootstrap-switch']).config(config);
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
     function config($stateProvider, $urlRouterProvider) {
         $stateProvider
@@ -13,6 +13,14 @@
             url: "/infos",
             templateUrl: "/app/components/info/InfoListView.html",
             controller: "InfoListController"
+        }).state('post', {
+            url: "/post",
+            templateUrl: "/app/components/post/PostListView.html",
+            controller: "postListController",
+        }).state('addpost', {
+            url: "/addpost",
+            templateUrl: "/app/components/post/PostAddView.html",
+            controller: "postAddController",
         });
         $urlRouterProvider.otherwise('/admin');
     }
---
+++ PhongTot/PhongTot.Administrator/app/components/post/postAddController.js
@@ -0,0 +1,47 @@
+﻿(function (app) {
+    app.controller('postAddController', postAddController);
+
+    postAddController.$inject = ['apiService', '$scope', 'notificationService', 'commonService'];
+
+    function postAddController(apiService, $scope, notificationService, commonService) {
+        $scope.postCategories = [];
+        $scope.post = {
+            Status: true
+        }
+        $scope.ckeditorOptions = {
+            languague: 'vi',
+            height: '200px'
+        }
+
+
+
+        $scope.GetSeoTitle = GetSeoTitle;
+
+        function GetSeoTitle() {
+            $scope.post.Alias = commonService.getSeoTitle($scope.post.Name);
+        }
+
+        $scope.AddPost = AddPost;
+
+        function AddPost() {
+            apiService.post('api/post/create', $scope.post,
+                function (result) {
+                    notificationService.displaySuccess(result.data.Name + 'đã được thêm mới.');
+                }, function (error) {
+                    notificationService.displayError('Thêm mới không thành công.');
+
+                });
+        }
+        function loadPostCategory() {
+            apiService.get('api/postcategory/getall', null, function (result) {
+                $scope.postCategories = result.data;
+            }, function () {
+                console.log('Cannot get list parent');
+            });
+        }
+
+        loadPostCategory();
+    }
+
+
+})(angular.module('myApp'));
