081a30ef6716aaad8693f233657ce7a9ef50f4d8
NamCkiku <tranhoangnam11373@gmail.com>
2016-12-07 10:52:21 +0700

    update syspara

---
+++ PhongTot/PhongTot.Administrator/app/components/SystemParameter/systemParameterEditController.js
@@ -0,0 +1,19 @@
+﻿(function (app) {
+    app.controller('systemParameterEditController', systemParameterEditController);
+    systemParameterEditController.$inject = ['apiService', '$scope', 'notificationService', '$timeout', '$window', 'blockUI'];
+    function systemParameterEditController(apiService, $scope, notificationService, $timeout, $window, blockUI) {
+        $scope.SaveData = SaveData;
+        function SaveData() {
+            $scope.arrSyspara = [];
+            $scope.arrSyspara.push({ Field: $("#chkshowsearch").attr("name"), Value: $("#chkshowsearch").attr('checked') });
+            $scope.arrSyspara = JSON.stringify($scope.arrSyspara);
+            apiService.put('api/syspara/update', $scope.arrSyspara,
+                function (result) {
+                    notificationService.displaySuccess('Chúc mừng bạn đã cập nhập thành công');
+                    $state.go('setting');
+                }, function (error) {
+                    notificationService.displayError('Cập nhật không thành công.');
+                });
+        }
+    }
+})(angular.module('myApp'));
---
+++ PhongTot/PhongTot.Api/Controllers/SysParaController.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json.Linq;
+using PhongTot.Api.Infrastructure.Core;
+using PhongTot.Entities.Models;
+using PhongTot.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PhongTot.Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/syspara")]
+    public class SysParaController : ApiControllerBase
+    {
+        private readonly ISysParaService _sysPaService;
+        public SysParaController(IErrorService errorService, ISysParaService sysPaService) : base(errorService)
+        {
+            this._sysPaService = sysPaService;
+        }
+        [Route("getall")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var syspara = _sysPaService.GetAll();
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, syspara);
+                return response;
+            });
+        }
+
+        [Authorize]
+        [Route("update")]
+        [HttpPut]
+        public HttpResponseMessage Put(HttpRequestMessage request, string lstSysparam)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var arrItem = JArray.Parse(lstSysparam);
+                    List<SysPara> lst = new List<SysPara>();
+                    foreach (var item in arrItem)
+                    {
+                        SysPara oSysPara = new SysPara();
+                        oSysPara.Field = item["Field"].ToString();
+                        oSysPara.Value = ((bool)item["Value"] == true) ? "1" : "0";
+                        lst.Add(oSysPara);
+                    }
+                    if (lst != null && lst.Count > 0)
+                    {
+                        if (_sysPaService.UpdateListSystemPara(lst))
+                        {
+                            IEnumerable<SysPara> listPara = _sysPaService.GetAll();
+                            response = request.CreateResponse(HttpStatusCode.OK, listPara);
+                        }
+                    }
+                    response = request.CreateResponse(HttpStatusCode.OK);
+
+
+                }
+                return response;
+            });
+        }
+    }
+}
