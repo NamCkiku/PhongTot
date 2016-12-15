ed683afbf28d211dd7c2484e6181d5fbb3aecfef
NamCkiku <tranhoangnam11373@gmail.com>
2016-12-15 22:40:40 +0700

    add formvalidation

--- PhongTot/PhongTot.Administrator/app/shared/services/apiService.js
+++ PhongTot/PhongTot.Administrator/app/shared/services/apiService.js
@@ -7,11 +7,41 @@
     function apiService($http, $location, notificationService, authenticationService) {
         var serviceBase = 'http://localhost:33029/';
         return {
+            ValidatorForm: ValidatorForm,
             get: get,
             post: post,
             put: put,
             del: del
         }
+        function ValidatorForm(form) {
+            $(form).formValidation({
+                framework: 'bootstrap',
+                icon: {
+
+                },
+                excluded: ':disabled',
+                fields: {
+
+                },
+            })
+            .off('success.form.fv')
+            .on('success.form.fv', function (e) {
+                var $form = $(e.target),
+                fv = $(e.target).data('formValidation');
+                fv.defaultSubmit();
+
+            })
+            .on('err.field.fv', function (e, data) {
+                if (data.fv.getSubmitButton()) {
+                    data.fv.disableSubmitButtons(false);
+                }
+            })
+            .on('success.field.fv', function (e, data) {
+                if (data.fv.getSubmitButton()) {
+                    data.fv.disableSubmitButtons(false);
+                }
+            });
+        };
         function del(url, data, success, failure) {
             authenticationService.setHeader();
             $http.delete(serviceBase + url, data).then(function (result) {
